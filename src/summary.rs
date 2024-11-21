5d9cb9e8cd61f0a590c622643f010efe3f1bbb17
Howie Benefiel <5847863+howinator@users.noreply.github.com>
2024-11-20 16:25:31 -0800

    get skeleton of LLM call in place

--- src/summary.rs
+++ src/summary.rs
@@ -56,6 +56,8 @@ impl Summarizer for ClaudeSummarizer {
     async fn summarize(&self, diff: &str) -> Result<String> {
         let c_summarizer = ClaudeSummarizer::new()?;
         let mut headers = HeaderMap::new();
+        let base_prompt = "Your job is to give a single sentence summary of the changes in this diff";
+        let full_prompt = [base_prompt, diff].join(" ");
         headers.insert("X-Api-Key", HeaderValue::from_str(&self.api_key)?);
         headers.insert("anthropic-version", "2023-06-01".parse()?);
         headers.insert("content-type", "application/json".parse()?);
@@ -63,13 +65,17 @@ impl Summarizer for ClaudeSummarizer {
             "model": "claude-3-5-haiku-20241022",
             "max_tokens": "256",
             "messages": [
-                {"role": "user", "content": }
+                {"role": "user", "content": full_prompt },
             ]
         }"#;
         let response = self.client.post("https://api.anthropic.com/v1/messages")
             .headers(headers)
             .json(&request_body)
-            .send()..json::<AnthropicAPIResponse>()?;
+            .send().await?;
+
+        let message: AnthropicAPIResponse = response.json().await?;
+
+        return Ok(message.content[0].text.clone());
 
         // We'll implement this next
     }
